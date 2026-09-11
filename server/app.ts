6f6b283a31ee8afcddc71598f62d53715742df66
laporejero <laporejero1297@gmail.com>
2026-09-11 22:39:25 +0800

    refactor: Extracted Express app into 'app.ts'

---
+++ server/app.ts
@@ -0,0 +1,20 @@
+import express, { Express } from 'express'
+
+import usersRouter from './controllers/users'
+import loginRouter from './controllers/login'
+import databasesRouter from './controllers/databases'
+import errorHandler from './middleware/errorHandler'
+import unknownEndpoint from './middleware/unknownEndpoint'
+
+const app: Express = express()
+
+app.use(express.json())
+
+app.use('/api/users', usersRouter)
+app.use('/api/login', loginRouter)
+app.use('/api/databases', databasesRouter)
+
+app.use(unknownEndpoint)
+app.use(errorHandler)
+
+export default app
--- server/index.ts
+++ server/index.ts
@@ -1,25 +1,10 @@
-import express, { Express } from 'express'
+import app from './app'
 import { PORT } from './util/config'
 import { connectToDatabase } from './util/db'
-import usersRouter from './controllers/users'
-import loginRouter from './controllers/login'
-import databasesRouter from './controllers/databases'
-import errorHandler from './middleware/errorHandler'
-import unknownEndpoint from './middleware/unknownEndpoint'
-
-const app: Express = express()
-
-app.use(express.json())
-
-app.use('/api/users', usersRouter)
-app.use('/api/login', loginRouter)
-app.use('/api/databases', databasesRouter)
-
-app.use(unknownEndpoint)
-app.use(errorHandler)
 
 const start = async (): Promise<void> => {
     await connectToDatabase()
+    
     app.listen(PORT, () => {
         console.log(`Server running on port ${PORT}`)
     })
