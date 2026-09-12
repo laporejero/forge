73961d34a1422e754bcfae977e5d9f59dda42ac1
laporejero <laporejero1297@gmail.com>
2026-09-12 21:39:50 +0800

    added more login API tests

--- server/tests/login.test.ts
+++ server/tests/login.test.ts
@@ -51,4 +51,207 @@ describe('POST /api/login', () => {
         expect(response.body.email).toBe('test@example.com')
         expect(response.body.name).toBe('Test User')
     })
+    test('successful login creates a session', async () => {
+        const credentials = {
+            email: 'test@example.com',
+            password: 'password123'
+        }
+
+        await api
+            .post('/api/login')
+            .send(credentials)
+            .expect(200)
+
+        const sessions = await Session.findAll()
+
+        expect(sessions).toHaveLength(1)
+        expect(sessions[0].token).toBeDefined()
+    })
+    test('successful login does not return passwordHash', async () => {
+        const credentials = {
+            email: 'test@example.com',
+            password: 'password123'
+        }
+
+        const response = await api
+            .post('/api/login')
+            .send(credentials)
+            .expect(200)
+
+        expect(response.body.passwordHash).toBeUndefined()
+    })
+    test('fails with 401 if password is incorrect', async () => {
+        const credentials = {
+            email: 'test@example.com',
+            password: 'wrongpassword'
+        }
+
+        const response = await api
+            .post('/api/login')
+            .send(credentials)
+            .expect(401)
+            .expect('Content-Type', /application\/json/)
+        
+        expect(response.body.error).toBe(
+            'Invalid email or password'
+        )
+    })
+    test('fails with 401 if user does not exist', async () => {
+        const credentials = {
+            email: 'missing@example.com',
+            password: 'password123'
+        }
+
+        const response = await api
+            .post('/api/login')
+            .send(credentials)
+            .expect(401)
+            .expect('Content-Type', /application\/json/)
+
+        expect(response.body.error).toBe(
+            'Invalid email or password'
+        )
+    })
+    test('fails with 400 if email is missing', async () => {
+        const credentials = {
+            password: 'password123'
+        }
+
+        const response = await api
+            .post('/api/login')
+            .send(credentials)
+            .expect(400)
+            .expect('Content-Type', /application\/json/)
+
+        expect(response.body.error).toBe(
+            'Email is required'
+        )
+    })
+    test('fails with 400 if email is empty', async () => {
+        const credentials = {
+            email: '',
+            password: 'password123'
+        }
+
+        const response = await api
+            .post('/api/login')
+            .send(credentials)
+            .expect(400)
+            .expect('Content-Type', /application\/json/)
+
+        expect(response.body.error).toBe(
+            'Email is required'
+        )
+    })
+    test('fails with 400 if email contains only whitespace', async () => {
+        const credentials = {
+            email: '     ',
+            password: 'password123'
+        }
+
+        const response = await api
+            .post('/api/login')
+            .send(credentials)
+            .expect(400)
+            .expect('Content-Type', /application\/json/)
+
+        expect(response.body.error).toBe(
+            'Email is required'
+        )
+    })
+    test('fails with 400 if email is invalid type', async () => {
+        const credentials = {
+            email: 12345,
+            password: 'password123'
+        }
+
+        const response = await api
+            .post('/api/login')
+            .send(credentials)
+            .expect(400)
+            .expect('Content-Type', /application\/json/)
+
+        expect(response.body.error).toBe(
+            'Invalid email'
+        )
+    })
+    test('fails with 400 if email is invalid format', async () => {
+        const credentials = {
+            email: 'not-an-email',
+            password: 'password123'
+        }
+
+        const response = await api
+            .post('/api/login')
+            .send(credentials)
+            .expect(400)
+            .expect('Content-Type', /application\/json/)
+
+        expect(response.body.error).toBe(
+            'Must be a valid email address'
+        )
+    })
+    test('fails with 400 if password is missing', async () => {
+        const credentials = {
+            email: 'test@example.com',
+        }
+
+        const response = await api
+            .post('/api/login')
+            .send(credentials)
+            .expect(400)
+            .expect('Content-Type', /application\/json/)
+
+        expect(response.body.error).toBe(
+            'Password is required'
+        )
+    })
+    test('fails with 400 if password is empty', async () => {
+        const credentials = {
+            email: 'test@example.com',
+            password: ''
+        }
+
+        const response = await api
+            .post('/api/login')
+            .send(credentials)
+            .expect(400)
+            .expect('Content-Type', /application\/json/)
+
+        expect(response.body.error).toBe(
+            'Password is required'
+        )
+    })
+    test('fails with 400 if password is invalid type', async () => {
+        const credentials = {
+            email: 'test@example.com',
+            password: 12345678
+        }
+
+        const response = await api
+            .post('/api/login')
+            .send(credentials)
+            .expect(400)
+            .expect('Content-Type', /application\/json/)
+
+        expect(response.body.error).toBe(
+            'Invalid password'
+        )
+    })
+    test('trim and lowercases email', async () => {
+        const credentials = {
+            email: ' TEST@EXAMPLE.COM ',
+            password: 'password123'
+        }
+
+        const response = await api
+            .post('/api/login')
+            .send(credentials)
+            .expect(200)
+            .expect('Content-Type', /application\/json/)
+
+        expect(response.body.token).toBeDefined()
+        expect(response.body.email).toBe('test@example.com')
+        expect(response.body.name).toBe('Test User')
+    })
 })
