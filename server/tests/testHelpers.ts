3bc175c950b24c2c05ffcb99fbd1a34b3c8674d5
laporejero <laporejero1297@gmail.com>
2026-09-17 15:20:04 +0800

    add clearTestDatabase helper

--- server/tests/testHelpers.ts
+++ server/tests/testHelpers.ts
@@ -1,7 +1,15 @@
 import request from 'supertest'
 import app from '../app'
+import { User, Database, Session, Field } from '../models'
 
 const api = request(app)
+
+export const clearTestDatabase = async () => {
+    await Session.destroy({ where: {} })
+    await Field.destroy({ where: {} })
+    await Database.destroy({ where: {} })
+    await User.destroy({ where: {} })
+} 
 
 export const postDatabase = async (token: string, name: string) => {
     return await api 
